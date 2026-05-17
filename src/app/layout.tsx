003ef64cb90f2c34b33934765493f7be4efe185c
gks-prog <gauravkumar41917@gmail.com>
2026-05-17 23:07:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/ui/Navbar";
+import Navbar from "@/components/ui/Navbar";
 import { CartDrawer } from "@/components/ui/CartDrawer";
 
 const inter = Inter({ subsets: ["latin"] });
