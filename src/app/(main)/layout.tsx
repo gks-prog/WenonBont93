0d7bae69ecb69d75a7a7f26c14e017f2538eb0ed
gks-prog <gauravkumar41917@gmail.com>
2026-05-17 09:27:18 +0530

    Update layout.tsx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,14 @@
 import { Navbar } from "@/components/ui/Navbar";
 import { CartDrawer } from "@/components/ui/CartDrawer";
+import { FloatingCartButton } from "@/components/ui/FloatingCartButton";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen bg-[#0a0a0a]">
       <Navbar />
       {children}
-      {/* Global Cart Injection */}
-      <CartDrawer /> 
+      <CartDrawer />
+      <FloatingCartButton />
     </div>
   );
 }
