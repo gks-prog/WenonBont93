eb3d05b5f83c4207255a6e48318c99349bad8546
gks-prog <gauravkumar41917@gmail.com>
2026-05-17 09:11:31 +0530

    Create layout.tsx

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components/ui/Navbar";
+import { CartDrawer } from "@/components/ui/CartDrawer";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative min-h-screen bg-[#0a0a0a]">
+      <Navbar />
+      {children}
+      {/* Global Cart Injection */}
+      <CartDrawer /> 
+    </div>
+  );
+}
